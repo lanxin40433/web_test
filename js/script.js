9c81df463797562e1475b02db0b1e7334732e3db
lanxin40433 <ada40433@gmail.com>
2022-05-26 15:24:35 +0800

    Fix JS.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const text = document.querySelectorAll(".thePaths");
+// const text = document.querySelectorAll(".thePaths");
 
-for (let i = 0; i < text.length; i++){
-    console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
-}
+// for (let i = 0; i < text.length; i++){
+//     console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
+// }
 
 const lastWord = document.querySelector("#twenty-third");
 const animation = document.querySelector("div.animation");
